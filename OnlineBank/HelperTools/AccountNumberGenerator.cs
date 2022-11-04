1f14f6b02a30c68695e403eac247464347084476
Aastha Grover <aasthagrover2000@gmail.com>
2022-11-04 17:18:03 +0530

    Adding helper tools

---
+++ OnlineBank/HelperTools/AccountNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace OnlineBank.API.HelperTools
+{
+    public class AccountNumberGenerator
+    {
+        public string acc()
+        {
+            Random random = new Random();
+            string r = "";
+            int i;
+            for (i = 1; i < 11; i++)
+            {
+                r += random.Next(1, 9).ToString();
+            }
+            return r;
+        }
+    }
+}
+
