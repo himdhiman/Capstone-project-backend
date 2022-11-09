427104007e2710baf31e52b638581d97426041fb
Aastha Grover <aasthagrover2000@gmail.com>
2022-11-09 21:56:09 +0530

    Added Phone Num Validation

---
+++ OnlineBank/HelperTools/ValidatePhoneNumber.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace OnlineBank.API.HelperTools
+{
+    public class ValidatePhoneNumber
+    {
+        public bool _numIsValid = false;
+        public const string motif = @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$";
+
+        public ValidatePhoneNumber(string phNum)
+        {
+            if (phNum!=null) _numIsValid= Regex.IsMatch(phNum, motif);
+        }
+    }
+}
+
